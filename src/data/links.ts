14586760420a38842bbeb146ae194ab03857eeee
ProjetosMautex <murilogxmautex@gmail.com>
2026-04-27 17:27:36 -0300

    Implement Google Ads conversion tracking on facasemoferta page

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,14 +1,14 @@
 export const productLinks = [
   {
     id: "Kit Rock Faca + Tabua Doma Crioula 8 Churrasco Cozinha Acdc",
-    link: "https://meli.la/1Dig7Rn"
+    link: "https://meli.la/1fN1kwK"
   },
   {
     id: "Faca Artesanal 10 Doma Crioula Cabo Escorpião Luxo Oferta",
-    link: "https://meli.la/1cmCLNw"
+    link: "https://meli.la/275US7o"
   },
   {
     id: "Faca Picanheira 8 Doma Crioula Sniper Dourada",
-    link: "https://meli.la/1va4rhP"
+    link: "https://meli.la/1WGoBpD"
   }
 ];
